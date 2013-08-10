b14c784369ed24cf39c8fc53d9e48aa29e5fbf03
Chris Peterson <chris@sparkdev.com>
2013-08-10 14:15:30 -0700

    Tweak NuGet packaging

--- src/Library/Properties/AssemblyInfo.cs
+++ src/Library/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Kekiri")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A .NET framework that supports writing low-ceremony BDD tests using Gherkin language")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Chris Peterson")]
 [assembly: AssemblyProduct("Kekiri")]
 [assembly: AssemblyCopyright("Copyright ©  2012-2013")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0")]
