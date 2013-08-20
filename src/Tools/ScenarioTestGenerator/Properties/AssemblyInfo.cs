804a1212f8e37d4da38ae214200de2aa00a08976
Chris Peterson <chris@sparkdev.com>
2013-08-19 18:25:28 -0700

    Get tools Chocolatey package working

--- src/Tools/ScenarioTestGenerator/Properties/AssemblyInfo.cs
+++ src/Tools/ScenarioTestGenerator/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Kekiri.ScenarioTestGenerator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A utility for generating a ScenarioTest from cucumber .feature text")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Chris Peterson")]
 [assembly: AssemblyProduct("Kekiri.ScenarioTestGenerator")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -49,5 +49,4 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.0")]
