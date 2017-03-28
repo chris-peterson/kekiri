03416b24ddf43d52db827c28d4a54cf1958071ef
Gurjit Basi <gurjit.basi@gettyimages.com>
2017-03-28 15:43:37 -0700

    Updated nunit to 3.6.1 and projects to new .net core version.

--- src/Kekiri/Properties/AssemblyInfo.cs
+++ src/Kekiri/Properties/AssemblyInfo.cs
@@ -1,38 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Kekiri")]
-[assembly: AssemblyDescription("A .NET framework that supports writing low-ceremony BDD tests using Gherkin language")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Chris Peterson")]
-[assembly: AssemblyProduct("Kekiri")]
-[assembly: AssemblyCopyright("Copyright © 2012-2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Kekiri.IoC.Autofac")]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("71b2ea37-d2e7-449f-8840-f437bab441e4")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("4.0.0")]
