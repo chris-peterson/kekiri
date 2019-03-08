e9761921fef931c0eec04cac93f785fb68641d35
sterling <sterling.smart@gettyimages.com>
2019-03-08 13:01:21 -0800

    Support custom IServiceProvider implementations

--- src/IoC/Kekiri.IoC.ServiceProvider/ServiceProviderBootstrapper.cs
+++ src/IoC/Kekiri.IoC.ServiceProvider/ServiceProviderBootstrapper.cs
@@ -12,10 +12,15 @@ namespace Kekiri.IoC
     {
         Type _startupType;
         Action<IServiceCollection> _config;
+        Action<IServiceCollection> _postStartupServicesConfig;
+        Action<object> _postStartupContainerBuilderConfig;
+        Type _containerType;
 
         public ServiceProviderBootstrapper()
         {
             _config = x => { };
+            _postStartupServicesConfig = x => { };
+            _postStartupContainerBuilderConfig = x => { };
         }
 
         public ServiceProviderBootstrapper UseStartup<TStartup>() where TStartup : class
@@ -29,9 +34,7 @@ namespace Kekiri.IoC
 
         public ServiceProviderBootstrapper OverrideServicesWithTypesFromAssemblyOf<T>() =>
             ConfigureServicesPostStartup(x =>
-                typeof(T).Assembly.GetTypes()
-                    .Where(y => !y.IsAbstract && !y.IsInterface)
-                    .SelectMany(ServiceDescriptors)
+                ServiceDescriptorsForConcreteTypes(typeof(T).Assembly)
                     .Aggregate(x, (s, y) => { s.Add(y); return s; }));
 
         static IEnumerable<ServiceDescriptor> ServiceDescriptors(Type type)
@@ -58,7 +61,7 @@ namespace Kekiri.IoC
             {
                 if (_startupType == null)
                 {
-                    config(x);
+                    _postStartupServicesConfig += config;
                 }
                 else
                 {
@@ -66,6 +69,43 @@ namespace Kekiri.IoC
                         new PostStartupConfigureServicesFilter(config));
                 }
             });
+
+        public ServiceProviderBootstrapper OverrideServicesWithTypesFromAssemblyUsingContainerBuilder<TContainerBuilder>(
+            Assembly assembly,
+            Action<TContainerBuilder, ServiceDescriptor> serviceRegistrationDelegate) 
+            => ConfigureServicesPostStartup<TContainerBuilder>(x =>
+                ServiceDescriptorsForConcreteTypes(assembly)
+                    .Aggregate(x, (s, y) => { serviceRegistrationDelegate.Invoke(s, y); return s; }));
+
+        public ServiceProviderBootstrapper OverrideServicesWithTypesFromAssemblyUsingContainerBuilder<TContainerBuilder>(
+            Assembly assembly,
+            Action<TContainerBuilder, IEnumerable<ServiceDescriptor>> serviceRegistrationDelegate)
+            => ConfigureServicesPostStartup<TContainerBuilder>(x =>
+                serviceRegistrationDelegate(x, ServiceDescriptorsForConcreteTypes(assembly)));
+
+        static IEnumerable<ServiceDescriptor> ServiceDescriptorsForConcreteTypes(Assembly assembly)
+            => assembly.GetTypes()
+                .Where(y => !y.IsAbstract && !y.IsInterface)
+                .SelectMany(ServiceDescriptors);
+
+        public ServiceProviderBootstrapper ConfigureServicesPostStartup<TContainerBuilder>(
+            Action<TContainerBuilder> config)
+        {
+            _containerType = typeof(TContainerBuilder);
+
+            return ConfigureServices(x =>
+            {
+                if (_startupType == null)
+                {
+                    _postStartupContainerBuilderConfig += y => config((TContainerBuilder) y);
+                }
+                else
+                {
+                    x.AddSingleton<IStartupConfigureContainerFilter<TContainerBuilder>>(y =>
+                        new PostStartupConfigureContainerFilter<TContainerBuilder>(config));
+                }
+            });
+        }
 
         public ServiceProviderBootstrapper ConfigureServices(Action<IServiceCollection> config)
         {
@@ -81,20 +121,57 @@ namespace Kekiri.IoC
             {
                 var services = new ServiceCollection();
 
-                    _config(services);
+                _config(services);
 
-                    result = services.BuildServiceProvider();
+                _postStartupServicesConfig(services);
+
+                result = services.BuildServiceProvider();
+
+                if (_containerType != null)
+                {
+                    result = BuildServiceProvider(result, services);
+                }
             }
             else
             {
                 // ReSharper disable once PossibleNullReferenceException
                 result = (IServiceProvider) typeof(ServiceProviderBootstrapper)
-                    .GetMethod(nameof(BuildServiceProvider),
-                        BindingFlags.Instance | BindingFlags.NonPublic)
+                    .GetMethod(
+                        nameof(BuildServiceProvider),
+                        BindingFlags.Instance | BindingFlags.NonPublic,
+                        binder: null,
+                        types: new Type[0],
+                        modifiers: null)
                     .GetGenericMethodDefinition().MakeGenericMethod(_startupType).Invoke(this, new object[0]);
             }
 
             return result;
+        }
+
+        /// <summary>
+        /// Builds an instance of <see cref="IServiceProvider"/> from an instance of <see cref="IServiceProviderFactory{TContainerBuilder}"/>.
+        /// </summary>
+        /// <param name="result"></param>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        IServiceProvider BuildServiceProvider(IServiceProvider result, ServiceCollection services)
+        {
+            var spfType = typeof(IServiceProviderFactory<>).MakeGenericType(_containerType);
+
+            var spf = result.GetRequiredService(spfType);
+
+            var containerBuilderFactory = spf.GetType().GetMethod(nameof(IServiceProviderFactory<object>.CreateBuilder));
+
+            // ReSharper disable once PossibleNullReferenceException
+            var containerBuilder = containerBuilderFactory.Invoke(spf, new object[] { services });
+
+            _postStartupContainerBuilderConfig.Invoke(containerBuilder);
+
+            var serviceProviderFactory = spf.GetType()
+                .GetMethod(nameof(IServiceProviderFactory<object>.CreateServiceProvider));
+
+            // ReSharper disable once PossibleNullReferenceException
+            return (IServiceProvider) serviceProviderFactory.Invoke(spf, new[] { containerBuilder });
         }
 
         IServiceProvider BuildServiceProvider<TStartup>() where TStartup : class
